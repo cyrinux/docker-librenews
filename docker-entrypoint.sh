7c186f7bc821e233f5b8380d02c208401d45eae9
Cyril Levis <cyril@levis.name>
2021-05-02 00:02:16 +0200

    Fix docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 echo "edit default settings from environment variables"
-declare -A CONFIG
 
-CONFIG["access_token"]=${TWITTER_ACCESS_TOKEN}
-CONFIG["access_token_secret"]=${TWITTER_ACCESS_TOKEN_SECRET}
-CONFIG["consumer_key"]=${TWITTER_CONSUMER_KEY}
-CONFIG["consumer_secret"]=${TWITTER_CONSUMER_SECRET}
-CONFIG["private_key"]=${VAPID_PRIVATE_KEY}
-CONFIG["public_key"]=${VAPID_PUBLIC_KEY}
-
-for KEY in ${CONFIG[@]}; do
-    if [ "${CONFIG[${KEY}]}" != "" ] ; then \
-        sed -i "s/\([ \t]*\"${KEY}\": \"\)[^\"]*\",/\1${CONFIG[${KEY}]//\//\\\/}\",/" /code/src/config.json
-    fi
-done
+sed -i "s/TWITTER_ACCESS_TOKEN_SECRET/${TWITTER_ACCESS_TOKEN_SECRET}/" -i /code/src/config.json
+sed -i "s/TWITTER_ACCESS_TOKEN/${TWITTER_ACCESS_TOKEN}/" -i /code/src/config.json
+sed -i "s/TWITTER_CONSUMER_SECRET/${TWITTER_CONSUMER_SECRET}/" -i /code/src/config.json
+sed -i "s/TWITTER_CONSUMER_KEY/${TWITTER_CONSUMER_KEY}/" -i /code/src/config.json
+sed -i "s/VAPID_PRIVATE_KEY/${VAPID_PRIVATE_KEY}/" -i /code/src/config.json
+sed -i "s/VAPID_PUBLIC_KEY/${VAPID_PUBLIC_KEY}/" -i /code/src/config.json
 
 /usr/bin/supervisord
